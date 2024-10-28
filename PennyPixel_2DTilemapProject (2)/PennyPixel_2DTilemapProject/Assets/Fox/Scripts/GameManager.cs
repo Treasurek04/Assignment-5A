80edfd259985f10428ade7c005ff4e89fea275ba
Treasurek04 <treasurekeys2004@gmail.com>
2024-10-28 00:35:52 -0500

    level design

---
+++ PennyPixel_2DTilemapProject (2)/PennyPixel_2DTilemapProject/Assets/Fox/Scripts/GameManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform player;
+    public float lossYThreshold = -10f;
+    public Text gameOverText;
+    public Text winText;
+
+    private bool hasLost = false;
+    private bool hasWon = false;
+
+    void Start()
+    {
+        gameOverText.gameObject.SetActive(false);
+        winText.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        CheckGameOverConditions();
+        HandleRetryInput();
+    }
+
+    private void CheckGameOverConditions()
+    {
+        if (player.position.y < lossYThreshold && !hasLost && !hasWon)
+        {
+            TriggerLoss();
+        }
+    }
+
+    private void HandleRetryInput()
+    {
+        if ((hasLost || hasWon) && Input.GetKeyDown(KeyCode.R))
+        {
+            Retry();
+        }
+    }
+
+    private void TriggerLoss()
+    {
+        hasLost = true;
+        gameOverText.text = "Game Over! Press 'R' to Retry";
+        gameOverText.gameObject.SetActive(true);
+    }
+
+    public void TriggerWin()
+    {
+        hasWon = true;
+        winText.text = "You Win! Press 'R' to Play Again";
+        winText.gameObject.SetActive(true);
+    }
+
+    private void Retry()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ PennyPixel_2DTilemapProject (2)/PennyPixel_2DTilemapProject/Assets/Fox/Scripts/ScoreManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    public Text scoreText;
+    private int score;
+    private GameManager gameManager;
+
+    void Start()
+    {
+        score = 0;
+        UpdateScoreUI();
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    public void AddScore(int points)
+    {
+        score += points;
+        UpdateScoreUI();
+
+        if (score == 10)
+        {
+            gameManager.TriggerWin();
+        }
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+        UpdateScoreUI();
+    }
+
+    private void UpdateScoreUI()
+    {
+        scoreText.text = "Score: " + score;
+    }
+}
