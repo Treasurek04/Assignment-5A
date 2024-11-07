b3f2d88a414450d7989389245eca38e99638d8d0
Treasurek04 <treasurekeys2004@gmail.com>
2024-11-06 22:26:22 -0600

    Update ScoreManager.cs

--- PennyPixel_2DTilemapProject (2)/PennyPixel_2DTilemapProject/Assets/Fox/Scripts/ScoreManager.cs
+++ PennyPixel_2DTilemapProject (2)/PennyPixel_2DTilemapProject/Assets/Fox/Scripts/ScoreManager.cs
@@ -1,4 +1,11 @@
-﻿using System.Collections;
+﻿/*
+ * Treasure Keys
+ * Assignment 5A
+ * Manage the Score UI and point system
+ 
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,7 +21,6 @@ public class ScoreManager : MonoBehaviour
     private int score;
     private GemCollision gemCollision;
 
-   
     void Start()
     {
         gameOver = false;
@@ -22,6 +28,7 @@ public class ScoreManager : MonoBehaviour
         score = 0;
 
         winText.gameObject.SetActive(false);
+        UpdateScore(); 
     }
 
     public void AddScore(int points)
@@ -29,23 +36,20 @@ public class ScoreManager : MonoBehaviour
         if (!gameOver)
         {
             score += points;
-
+            UpdateScore();
         }
+
         if (score >= 10)
         {
-
             GameOver(true);
             winText.text = "You Win! Press 'R' to Play Again";
             winText.gameObject.SetActive(true);
-
         }
     }
 
     private void UpdateScore()
     {
         scoreText.text = "Score: " + score;
-
-       
     }
 
     void GameOver(bool result)
@@ -56,13 +60,9 @@ public class ScoreManager : MonoBehaviour
 
     void Update()
     {
-        
-            if (Input.GetKeyDown(KeyCode.R))
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-            }
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 }
-
-  
